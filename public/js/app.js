e951a79321f97deff13d2e0bf49bf8bada0f1157
Jorge Felico <jorgefelico@gmail.com>
2017-03-28 08:57:24 -0400

    Added ngMaterial as well as a loading spinner

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 // Create initial angular app
-var app = angular.module('TreehouseTimeline', ['ngAnimate']);
+var app = angular.module('TreehouseTimeline', ['ngAnimate', 'ngMaterial']);
 
 // Create factory for communicating with API
 app.factory('ProfileFactory', function($http){
@@ -30,16 +30,24 @@ app.directive('profileView', function(){
 // Create our profile controller
 app.controller('profile', function($scope, ProfileFactory){
   $scope.gotData = false;
-  $scope.profileData = {};
+  $scope.loadingData = false;
+  $scope.showError = false;
 
   $scope.getProfile = function(){
+    $scope.showError = false;
+    $scope.loadingData = true;
+    $scope.gotData = false;
+
     ProfileFactory.getProfile($scope.username).then(function(response){
       $scope.profileData = {};
       $scope.gotData = true;
       $scope.profileData = response.data;
+      $scope.loadingData = false;
 
     }, function(response){
       console.log(response.toString());
+      $scope.loadingData = false;
+      $scope.showError = true;
     });
   }
 
